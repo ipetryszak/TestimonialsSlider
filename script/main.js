a8cd190daaba35048027cb2b4c26bbaf8cf6b6b5
ireneusz <ireneusz.petryszak@gmail.com>
2020-10-12 23:17:07 +0200

    js modified

--- script/main.js
+++ script/main.js
@@ -2,34 +2,14 @@ let num = 0;
 const el = document.querySelectorAll('.slide-element--1');
 
 function nextSlide(e) {
-    if(num < el.length - 1)
-    {
-        num++;
-        changeSlide(e,num);
-    }
-    else
-    {
-        num = 0;
-        changeSlide(e,num);
-    }
-
+    num < (el.length-1) ? changeSlide(e,++num) : changeSlide(e,0);
 }
 
 function prevSlide(e) {
-    if(num>0)
-    {
-        num--;
-        changeSlide(e,num);
-    }
-    else {
-        num = el.length - 1;
-        changeSlide(e,num);
-    }
+    num > 0 ? changeSlide(e,--num) : changeSlide(e,el.length - 1);
 }
 
-
 function changeSlide(e,num) {
-        console.log(el, num);
         for(let i = 0; i < el.length;i++)
         {
             el[i].style.display = 'none';
